92f46009208e978cc15f9b483776550922be04f4
prash <prash@Prashant-Lenevo.attlocal.net>
2022-07-28 18:44:58 -0400

    Base testsaved

--- src/test/java/acti/tests/BaseTests.java
+++ src/test/java/acti/tests/BaseTests.java
@@ -2,11 +2,13 @@ package acti.tests;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 
 import acti.driver.DriverManager;
 import acti.pages.EnterPage;
 import acti.pages.LoginPage;
 import acti.pages.TaskPage;
+import acti.utils.ExcelLib;
 
 
 public class BaseTests extends DriverManager {
@@ -28,4 +30,28 @@ public class BaseTests extends DriverManager {
 		quit_browser();
 	}
 
+	@DataProvider(name = "actidata")
+	public Object[][] testData() {
+
+		ExcelLib x1 = new ExcelLib("./src/test/resources/acti/testdata/Actidata.xlsx"); // passing the path of the excel file
+
+		int rows = x1.getRowCount(0);
+
+		Object data[][] = new Object[rows][2]; // creating an object class of twodimensional array
+		for (int i = 0; i < rows; i++) { // iterating through rows
+			data[i][0] = x1.getCellData(0, i, 0);
+			data[i][1] = x1.getCellData(0, i, 1); //
+		}
+
+		return data;
+	}
+
 }
+	
+	
+	  
+	  
+	  
+	 
+
+
