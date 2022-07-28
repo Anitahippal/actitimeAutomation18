0aff43ad28e62397e812ef81014e7eef7b64959b
prash <prash@Prashant-Lenevo.attlocal.net>
2022-07-28 15:50:51 -0400

    Saved and commit th enter time page

--- src/main/java/acti/pages/EnterPage.java
+++ src/main/java/acti/pages/EnterPage.java
@@ -8,6 +8,15 @@ import acti.driver.DriverManager;
 
 //don’t hard code any of the test data in the page  classes
 
+/*
+ * Name :Enter Time Page
+ * Developed By Anita
+ * Approved By    Hippalgaonkar
+ * Last Modified Date 7/26/2022
+ * Description : Enter Time page (home page) elements and its methods
+ */
+
+//************************* the page elements*************************************/
 
 public class EnterPage extends DriverManager{
 	@FindBy(id ="logoutLink") WebElement linklogout;
@@ -15,12 +24,14 @@ public class EnterPage extends DriverManager{
 	@FindBy(id = "container_tasks") WebElement MenuTask;
 
 
-
+	// *****************************Page	// Initialization*****************************//
 	
 	public EnterPage()  { //constructor is made
 		PageFactory.initElements(driver, this);
 	}
 	
+	
+	// *****************************Page Actions*****************************//
 	 public void  clickTask(){
 		 MenuTask.click();
 	 }
