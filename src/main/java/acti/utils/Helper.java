c52694b5c07d4d42775b28baa8b2d5748b255363
prash <prash@Prashant-Lenevo.attlocal.net>
2022-07-28 15:53:00 -0400

    commit with thread.sleep

--- src/main/java/acti/utils/Helper.java
+++ src/main/java/acti/utils/Helper.java
@@ -3,5 +3,16 @@ package acti.utils;
 import acti.driver.DriverManager;
 
 public class Helper extends DriverManager {
+	
+	public static void fn_sleep() {
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 
 }
